12e81b18c27612f6c1eea64dd320d37d348ebf53
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-16 11:41:11 +0100

    add map cycle to render the array on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-
 import './App.css'
-
+import { useState, useEffect } from "react"
 function App() {
+
+  const movies = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
 
 
   return (
     <>
-  
+      <div className="container">
+        <h1>Questa è la tua lista di film</h1>
+        <ul>
+          {movies.map(movie => (<li>Titolo del film: {movie.title} - Genere: {movie.genre}</li>))}
+        </ul>
+      </div>
     </>
   )
 }
