f70a45c88817da9c47d06db8338c6c41787799f5
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-16 15:41:03 +0100

    add searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -20,30 +20,69 @@ function App() {
       setFilteredGenre(movies.filter(movie => (movie.genre === genre)))
     }
   }, [genre, movies])
+  const [selectedMovie, setSelectedMovie] = useState("")
 
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const searchResult = movies.filter(movie => movie.title.toLowerCase().includes(selectedMovie.toLowerCase()))
+    setFilteredGenre(searchResult)
+    setSelectedMovie("")
+  }
 
 
   return (
     <>
-      <div className="container">
-        <h1>Questa è la tua lista di film</h1>
-          <select value={genre} name="movies" id="movies" onChange={(e) => setGenre(e.target.value)}>
-          <option value="">Seleziona un genere per filtrare</option>
-          <option value="Fantascienza">Fantascienza</option>
-          <option value="Thriller">Thriller</option>
-          <option value="Romantico">Romantico</option>
-          <option value="Azione">Azione</option>
-        </select>
-        <ul>
+      <header>
+        <h1 className="title text-center text-white"> Cineplex Movie List Filter 🍿</h1>
+      </header>
+      <main>
+        <div className="container">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Inserisci nome del film..."
+              className="search"
+              value={selectedMovie}
+              onChange={(e) => setSelectedMovie(e.target.value)}
+            />
+            <button type="submit" className="button mx-1">Cerca</button>
+          </form>
+
+          <select value={genre} name="movies" id="movies" onChange={(e) => setGenre(e.target.value)} className="form-select my-4" >
+            <option value="">Seleziona un genere</option>
+            <option value="Fantascienza">Fantascienza</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Romantico">Romantico</option>
+            <option value="Azione">Azione</option>
+          </select>
           {filteredGenre.map(movie => (
-            
-            
-            <li key={movie.title}>Titolo del film: {movie.title} - Genere: {movie.genre}</li>
-        ))}
-        </ul>
-      </div>
+            <table key = {movie.title} className="table table-striped table-dark">
+              <thead>
+                <tr>
+                  <th scope="col">Nome</th>
+                  <th scope="col">Genere</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">{movie.title}</th>
+                  <td>{movie.genre}</td>
+                </tr>
+              </tbody>
+            </table>
+          ))}
+        </div>
+      </main>
+      <footer>
+        <div className="footer text-center text-white">
+          <span className="span h6">© Cineplex Movie List 2026 --- Made with some heachache by Christian Matarazzo</span>
+        </div>
+      </footer>
+
+
     </>
   )
-}
 
+}
 export default App
