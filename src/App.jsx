5e8aa3b0607b6ff3be16cda954885f118b4f5363
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-16 12:45:43 +0100

    add filtered list

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
 import './App.css'
 import { useState, useEffect } from "react"
+const movies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 function App() {
+  const [genre, setGenre] = useState("")
+  const [filteredGenre, setFilteredGenre] = useState(movies)
 
-  const movies = [
-    { title: 'Inception', genre: 'Fantascienza' },
-    { title: 'Il Padrino', genre: 'Thriller' },
-    { title: 'Titanic', genre: 'Romantico' },
-    { title: 'Batman', genre: 'Azione' },
-    { title: 'Interstellar', genre: 'Fantascienza' },
-    { title: 'Pulp Fiction', genre: 'Thriller' },
-  ]
+  useEffect(() => {
+    if (genre === "") {
+      setFilteredGenre(movies)
+    }
+    else {
+      setFilteredGenre(movies.filter(movie => (movie.genre === genre)))
+    }
+  }, [genre, movies])
+
 
 
   return (
     <>
       <div className="container">
         <h1>Questa è la tua lista di film</h1>
+          <select value={genre} name="movies" id="movies" onChange={(e) => setGenre(e.target.value)}>
+          <option value="">Seleziona un genere per filtrare</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+        </select>
         <ul>
-          {movies.map(movie => (<li>Titolo del film: {movie.title} - Genere: {movie.genre}</li>))}
+          {filteredGenre.map(movie => (
+            
+            
+            <li key={movie.title}>Titolo del film: {movie.title} - Genere: {movie.genre}</li>
+        ))}
         </ul>
       </div>
     </>
